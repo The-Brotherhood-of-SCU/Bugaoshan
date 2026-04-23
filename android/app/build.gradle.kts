647c9b2bffe0efff0deb662b40ccd88352d57157
jeanhua <2207739460@qq.com>
2026-04-23 21:58:52 +0800

    refactor: 修改应用包名为小写格式
    将各平台的应用ID从"io.github.the_brotherhood_of_scu.Bugaoshan"修改为小写格式"io.github.the_brotherhood_of_scu.bugaoshan"，保持命名一致性

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "io.github.the_brotherhood_of_scu.Bugaoshan"
+    namespace = "io.github.the_brotherhood_of_scu.bugaoshan"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -29,7 +29,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "io.github.the_brotherhood_of_scu.Bugaoshan"
+        applicationId = "io.github.the_brotherhood_of_scu.bugaoshan"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
