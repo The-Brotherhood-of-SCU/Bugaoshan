6dc4cd77fe5c229a4bbcf01299b1387721a75fe0
57U <ming57u@foxmail.com>
2026-05-03 00:12:52 +0800

    fix: resolve Android compilation errors
    - Fix Koin imports to use org.koin package (not io.koin)
    - Add koin-android library entry to version catalog
    - Set Android SDK path in local.properties
    - Remove androidLogger() which requires Level parameter in Koin 4.x
    - Add server/bin/ to .gitignore

--- composeApp/src/androidMain/kotlin/io/github/the_brotherhood_of_scu/bugaoshan/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/io/github/the_brotherhood_of_scu/bugaoshan/MainActivity.kt
@@ -9,9 +9,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import io.github.the_brotherhood_of_scu.bugaoshan.di.appModule
 import io.github.the_brotherhood_of_scu.bugaoshan.di.viewModelModule
 import io.github.the_brotherhood_of_scu.bugaoshan.platform.appContext
-import io.koin.android.ext.koin.androidContext
-import io.koin.android.ext.koin.androidLogger
-import io.koin.core.context.startKoin
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,11 +20,12 @@ class MainActivity : ComponentActivity() {
         // Initialize app context for platform-specific code
         appContext = applicationContext
 
-        // Initialize Koin
+        // Initialize Koin with Android context module
+        val androidModule = module {
+            single { this@MainActivity }
+        }
         startKoin {
-            androidContext(this@MainActivity)
-            androidLogger()
-            modules(appModule, viewModelModule)
+            modules(appModule, viewModelModule, androidModule)
         }
 
         setContent {
