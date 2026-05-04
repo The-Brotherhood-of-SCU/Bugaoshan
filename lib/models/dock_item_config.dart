cb17f412fc2915c81e253a5286cf211befd1042f
57U <ming57u@foxmail.com>
2026-05-04 21:46:01 +0800

    rm uselesss property

--- lib/models/dock_item_config.dart
+++ lib/models/dock_item_config.dart
@@ -6,13 +6,11 @@ class DockItemConfig {
   final String id;
   final int iconCodePoint;
   final int selectedIconCodePoint;
-  final String labelKey;
 
   const DockItemConfig({
     required this.id,
     required this.iconCodePoint,
     required this.selectedIconCodePoint,
-    required this.labelKey,
   });
 
   IconData get icon => IconData(iconCodePoint, fontFamily: 'MaterialIcons');
@@ -23,7 +21,6 @@ class DockItemConfig {
         'id': id,
         'iconCodePoint': iconCodePoint,
         'selectedIconCodePoint': selectedIconCodePoint,
-        'labelKey': labelKey,
       };
 
   factory DockItemConfig.fromJson(Map<String, dynamic> json) =>
@@ -31,7 +28,6 @@ class DockItemConfig {
         id: json['id'] as String,
         iconCodePoint: json['iconCodePoint'] as int,
         selectedIconCodePoint: json['selectedIconCodePoint'] as int,
-        labelKey: json['labelKey'] as String,
       );
 }
 
@@ -40,67 +36,56 @@ List<DockItemConfig> allDockItems() => [
         id: dockIdCourse,
         iconCodePoint: Icons.menu_book_outlined.codePoint,
         selectedIconCodePoint: Icons.menu_book.codePoint,
-        labelKey: dockIdCourse,
       ),
       DockItemConfig(
         id: dockIdCampus,
         iconCodePoint: Icons.school_outlined.codePoint,
         selectedIconCodePoint: Icons.school.codePoint,
-        labelKey: dockIdCampus,
       ),
       DockItemConfig(
         id: dockIdProfile,
         iconCodePoint: Icons.person_outlined.codePoint,
         selectedIconCodePoint: Icons.person.codePoint,
-        labelKey: dockIdProfile,
       ),
       DockItemConfig(
         id: dockIdGrades,
         iconCodePoint: Icons.bar_chart_outlined.codePoint,
         selectedIconCodePoint: Icons.bar_chart.codePoint,
-        labelKey: dockIdGrades,
       ),
       DockItemConfig(
         id: dockIdCcyl,
         iconCodePoint: Icons.event_outlined.codePoint,
         selectedIconCodePoint: Icons.event.codePoint,
-        labelKey: dockIdCcyl,
       ),
       DockItemConfig(
         id: dockIdPlanCompletion,
         iconCodePoint: Icons.assignment_turned_in_outlined.codePoint,
         selectedIconCodePoint: Icons.assignment_turned_in.codePoint,
-        labelKey: dockIdPlanCompletion,
       ),
       DockItemConfig(
         id: dockIdTrainProgram,
         iconCodePoint: Icons.school_outlined.codePoint,
         selectedIconCodePoint: Icons.school.codePoint,
-        labelKey: dockIdTrainProgram,
       ),
       DockItemConfig(
         id: dockIdClassroom,
         iconCodePoint: Icons.meeting_room_outlined.codePoint,
         selectedIconCodePoint: Icons.meeting_room.codePoint,
-        labelKey: dockIdClassroom,
       ),
       DockItemConfig(
         id: dockIdNetworkDevice,
         iconCodePoint: Icons.router_outlined.codePoint,
         selectedIconCodePoint: Icons.router.codePoint,
-        labelKey: dockIdNetworkDevice,
       ),
       DockItemConfig(
         id: dockIdBalanceQuery,
         iconCodePoint: Icons.account_balance_wallet_outlined.codePoint,
         selectedIconCodePoint: Icons.account_balance_wallet.codePoint,
-        labelKey: dockIdBalanceQuery,
       ),
       DockItemConfig(
         id: dockIdAcademicCalendar,
         iconCodePoint: Icons.calendar_month_outlined.codePoint,
         selectedIconCodePoint: Icons.calendar_month.codePoint,
-        labelKey: dockIdAcademicCalendar,
       ),
     ];
 
