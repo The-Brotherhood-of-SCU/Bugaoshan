ca95eb3cd79fc7770d1d7e2165834614ab5b15d3
57U <ming57u@foxmail.com>
2026-05-05 19:30:07 +0800

    fix(exit_service): add delay before app exit to ensure data persistence

--- lib/services/exit_service.dart
+++ lib/services/exit_service.dart
@@ -8,6 +8,8 @@ class ExitService {
   /// 桌面端使用 windowManager.destroy() 正确关闭窗口
   /// 移动端使用 exit(0) 直接退出
   Future<void> exitApp() async {
+    //make sure changes is saved
+    await Future.delayed(Duration(milliseconds: 300));
     if (!kIsWeb &&
         (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
       await windowManager.destroy();
