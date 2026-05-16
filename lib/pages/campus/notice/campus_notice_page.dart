c8845e355f93952d914180f4edadb44a66b09618
jeanhua <2207739460@qq.com>
2026-05-16 17:18:27 +0800

    feat(campus notice): add open_filex dependency and replace file launch method
    替换了原有的url_launcher文件打开方式为open_filex，统一并简化附件打开逻辑，新增open_filex第三方依赖

--- lib/pages/campus/notice/campus_notice_page.dart
+++ lib/pages/campus/notice/campus_notice_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:gal/gal.dart';
 import 'package:http/http.dart' as http;
+import 'package:open_filex/open_filex.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 import 'package:photo_view/photo_view.dart';
