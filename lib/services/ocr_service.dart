f6c79132ee54f6b3fe114e45a2c64beafcdc6a94
Under_Tree <jimdeng2022@gmail.com>
2026-09-21 11:14:28 +0800

    fix: 兼容 Flutter Web 验证码 OCR

--- lib/services/ocr_service.dart
+++ lib/services/ocr_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:scu_ocr_lite/scu_ocr_lite.dart' as ocr_lite;
 
@@ -26,6 +27,9 @@ class OcrService {
 
   static Future<String> performOcr(Uint8List imageBytes) async {
     await init();
+    // 依赖库的 recognizeAsync 使用 Isolate.run，Flutter Web 不支持。
+    // Web 复用同一份本地模型直接识别，原生平台仍在 isolate 中执行。
+    if (kIsWeb) return _instance!.recognize(imageBytes);
     return _instance!.recognizeAsync(imageBytes);
   }
 }
